da5c9464c4e9df4a3803b452a527fe5a688c6498
khaismile1997 <khaismile1997@gmail.com>
2021-12-30 01:02:41 +0700

    show user info & add callback before save user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   has_many :articles
+
   validates :username, presence: true, uniqueness: {case_sensitive: false}
   validates :username, length: {minimum: 3, maximum: 25}, if: :username?
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
