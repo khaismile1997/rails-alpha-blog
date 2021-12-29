27a13222d932f116ee95112af6beb78d385150fd
khaismile1997 <khaismile1997@gmail.com>
2021-12-30 00:49:37 +0700

    add association user - article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}
   validates :username, length: {minimum: 3, maximum: 25}, if: :username?
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
